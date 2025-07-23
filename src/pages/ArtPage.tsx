4fac412cd4eca66377b7187e07a396b8fc503de4
Suha Mirza <suhamirza1@gmail.com>
2025-07-24 01:59:03 +0300

    ensured proper automatic scrolling to top of navigated page

--- src/pages/ArtPage.tsx
+++ src/pages/ArtPage.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Art from '../components/Art';
 import Footer from '../components/Footer';
 
 const ArtPage: React.FC = () => {
+  useEffect(() => {
+    // Scroll to top when component mounts
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="min-h-screen">
       {/* No Navbar here since it's already in App.tsx */}
--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Books from '../components/Books';
 import Footer from '../components/Footer';
 
 const BooksPage: React.FC = () => {
+  useEffect(() => {
+    // Scroll to top when component mounts
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="min-h-screen">
       {/* No Navbar here since it's already in App.tsx */}
