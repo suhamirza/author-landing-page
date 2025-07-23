5daa0b0456d4adf010911d5930da37d246df01e0
Suha Mirza <suhamirza1@gmail.com>
2025-07-24 01:15:25 +0300

    made changes for custom domain

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/author-landing-page">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
