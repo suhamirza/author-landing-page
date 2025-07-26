3b8fb3fe7098ab740e1ce5dd718bc1c33f010b0c
Suha Mirza <suhamirza1@gmail.com>
2025-07-26 03:04:07 +0300

    tried fixing the dublin sisterverse navigation from footer

--- src/pages/ArtPage.tsx
+++ src/pages/ArtPage.tsx
@@ -1,12 +1,32 @@
 import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Art from '../components/Art';
 import Footer from '../components/Footer';
 
 const ArtPage: React.FC = () => {
+  const location = useLocation();
+
   useEffect(() => {
-    // Scroll to top when component mounts
-    window.scrollTo(0, 0);
-  }, []);
+    // Check if there's a hash in the URL
+    if (location.hash) {
+      // Wait a bit for the page to render, then scroll to the element
+      setTimeout(() => {
+        const element = document.getElementById(location.hash.substring(1));
+        if (element) {
+          const navbarHeight = 60; // Account for fixed navbar height
+          const targetPosition = element.offsetTop - navbarHeight;
+          
+          window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth'
+          });
+        }
+      }, 100);
+    } else {
+      // No hash, scroll to top
+      window.scrollTo(0, 0);
+    }
+  }, [location.hash]);
 
   return (
     <div className="min-h-screen">
