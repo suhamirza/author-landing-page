8418407ba721fa4bec2c40a273466122687675fd
Suha Mirza <suhamirza1@gmail.com>
2025-09-08 21:12:31 +0300

    seo stuff 2.0

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,7 +27,7 @@ const Home: React.FC = () => {
   return (
     <div>
       <SEO 
-        title="Hayat Khan - Author of The Dublin Sisters Series"
+        title="Hayat Khan - The Dublin Sisters Series Author"
         description="Welcome to the official website of Hayat Khan, author of the Dublin Sisters series. Discover captivating novels featuring strong characters, beautiful storytelling, and immersive world-building."
         url="https://hayatkhantheauthor.com"
         image="https://hayatkhantheauthor.com/assets/bookone.jpg"
